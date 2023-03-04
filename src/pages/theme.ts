7c630282fa23b95b93b1842e7255082859d80cdc
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2023-03-04 18:08:22 +0530

    changed export in theme.js

--- src/pages/theme.ts
+++ src/pages/theme.ts
@@ -10,7 +10,7 @@ export const roboto = Roboto({
 });
 
 // Create a theme instance.
-const theme = createTheme({
+export const theme = createTheme({
     palette: {
         primary: {
             main: '#556cd6',
@@ -27,4 +27,3 @@ const theme = createTheme({
     },
 });
 
-export default theme;
