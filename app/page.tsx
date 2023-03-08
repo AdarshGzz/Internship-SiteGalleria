d46f53036e407bcc8b1b3037d84e8ce5dfea5287
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2023-03-08 13:50:26 +0530

    Shifted the project to app directory of nextjs 13

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from 'react'
+import Header from './Components/Header';
+import Carousel from './Components/Carousel/Carousel';
+
+const Home = () => {
+  return (
+    <div>
+          <Header />
+          <div>
+              <Carousel />
+              {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quibusdam maxime quisquam consectetur possimus, dolorum suscipit reiciendis debitis atque tempora ab alias doloremque quidem facilis eius. Rerum id quo officiis! */}
+          </div>
+    </div>
+  )
+}
+
+export default Home
