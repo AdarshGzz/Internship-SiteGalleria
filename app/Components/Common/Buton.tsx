f5d8a46558eb95075c11e8e8ffc144e564d62c62
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2023-03-09 13:53:14 +0530

    created commonly used button

---
+++ app/Components/Common/Buton.tsx
@@ -0,0 +1,74 @@
+import { Box } from '@mui/system'
+import React from 'react'
+
+interface Props{
+  children?:React.ReactNode,
+  Height?:number,
+  Width?:number,
+  Color?:string, 
+  FontSize?:number, 
+  FontWeight?:number, 
+  Rounded?:number, 
+  Type?: 'box' | 'bullet',
+}
+
+const Buton:React.FC<Props> = ({children,Height,Width,Color,FontSize,FontWeight,Rounded,Type}) => {
+
+  let H: string = '3.4rem'
+  let W:string = '15rem'
+  let colr:string = '#6440FB'
+  let fntsz:string = '1.4rem'
+  let fntwt:string = '300'
+  let radius:string = Type==='bullet'?'100px':Type==='box'?'8px':'2px'
+
+  if(Height){
+    H = `${Height}rem`
+  }
+  if(Width){
+    W = `${Width}rem`
+  }
+  if(Color){
+    colr = `${Color}`
+  }
+  if(FontSize){
+    fntsz = `${FontSize}rem`
+  }
+  if(FontWeight){
+    fntwt = `${FontWeight}`
+  }
+  if(Rounded){
+    radius = `${Rounded}rem`
+  }
+
+
+  return (
+    <div>
+      <Box
+        sx={{
+          height:`${H}`,
+          width:`${W}`,
+          color:`${colr}`,
+          fontSize:`${fntsz}`,
+          fontWeight:`${fntwt}`,
+          borderRadius:`${radius}`,
+          display:'flex',
+          alignItems:`center`,
+          justifyContent:`center`,
+          padding:'14px 30px',
+          border:2,
+          borderColor:`${colr}`,
+          transitionDuration:`.2s`,
+          cursor:'pointer',
+          '&:hover': {
+            color:'#fff',
+            backgroundColor:`${colr}`
+          },
+        }}
+      >
+        {children}
+      </Box>
+    </div>
+  )
+}
+
+export default Buton
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import React from 'react'
 import Header from './Components/Header';
 import Carousel from './Components/Carousel/Carousel';
 import Heading from './Components/Common/Heading';
+import Buton from './Components/Common/Buton';
 
 const Home = () => {
   return (
     <div>
           <Header />
           <Carousel />
-          <Heading Subheading ={'Lorem ipsum dolor sit amet, consectetur.'} >Top Students</Heading>
-
+          {/* <Heading Subheading ={'Lorem ipsum dolor sit amet, consectetur.'} >Top Students</Heading> */}
+          {/* <Buton Type='bullet' >hello</Buton> */}
     </div>
   )
 }
